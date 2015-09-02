2e09630cae23c1347fb80caf8b65253f1a9409bd
Matthew Deitz <mdeitz@uncc.edu>
2015-09-02 14:23:30 -0400

    kdtc

--- src/examples/Lab2ExampleExtraCredit.java
+++ src/examples/Lab2ExampleExtraCredit.java
@@ -7,46 +7,50 @@ public class Lab2ExampleExtraCredit
 	public static void main(String[] args) 
 	{
 		Random random = new Random();
-		int counter = 0;
+		int[] totalCount = new int[10000];
 		
-		for(int n=0;n<1000;n++)
+		for (int l =0;l<10000;l++)
 		{
-			String threeMer = "";
-			for(int m = 0; m<3;m++)
-			{
-				float f = random.nextFloat();
-				if (f<=.12)
-				{
-					threeMer +="A";
-				}
-				else if (f <= .50)
-				{
-					threeMer += "C";
-				}
-				else if (f <= .89)
-				{
-					threeMer += "G";
-				}
-				else
-				{
-					threeMer += "T";
-				}
-			}
-			if (threeMer.equals("AAA"))
-			{
-				counter++;
-			}
-			System.out.println(threeMer);
+			int counter = 0;
 			
-		}
-		System.out.println("The three-mer AAA appears "+counter+" times.");
+			for(int n=0;n<10000;n++)
+			{
+				String threeMer = "";
+				for(int m = 0; m<3;m++)
+				{
+					float f = random.nextFloat();
+					if (f<=.12)
+					{
+						threeMer +="A";
+					}
+					else if (f <= .50)
+					{
+						threeMer += "C";
+					}
+					else if (f <= .89)
+					{
+						threeMer += "G";
+					}
+					else
+					{
+						threeMer += "T";
+					}
+				}
+				
+				if (threeMer.equals("AAA"))
+				{
+					counter++;
+				}				
+			}
+			totalCount[l]=counter;
 	/*
-	 * Theoretical value should be around 1-2 Times
-	 * P1*P2*P3*1000
+	 * Theoretical value should be around 17-18 Times
+	 * P1*P2*P3*10000
 	 * PN = probability of N being an A = .12
-	 * = .12*.12*.12*1000 = 1.728
+	 * = .12*.12*.12*10000 = 17.28
 	 */
-	
+		System.out.println("Run #"+(l+1)+" has "+totalCount[l]+" times AAA occured");	
+		}
 	}
 
 }
